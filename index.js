51f3e172f54531e8efd4aeaae423b8d9669618a7
Trevor Russell <truss.42@gmail.com>
2018-05-17 21:45:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function theBeatlesPlay(musicians, intruments) {
   var a = [];
-  for (var i = 0; i <= musicians.length - 1; i++) {
-    a.push(`"${musicians[i]} plays ${intruments[i]}"`)
+  for (var i = 0; i < musicians.length; i++) {
+    a.push(`${musicians[i]} plays ${intruments[i]}`)
   }
   return a
+}
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i < facts.length){
+    facts[i] = facts[i] + "!!!";
+    i++
+  }
+  return facts
 }
